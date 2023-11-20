c45c5cb440c46ed014362403c56480f124012d3a
Kenneth-Parker <kennethkavon@gmail.com>
2023-11-20 00:22:48 -0500

    updated order

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,16 @@ CREATE DATABASE resources;
 
 \c resources;
 
+
+-- Table: locations
+CREATE TABLE locations (
+  location_id SERIAL PRIMARY KEY,
+  location_name TEXT NOT NULL,
+  city TEXT,
+  state CHAR(2) CHECK (CHAR_LENGTH(state) = 2),
+  zip_code VARCHAR(5) CHECK (zip_code ~ '^[0-9]{5}$')
+);
+
 -- Table: resources
 CREATE TABLE resources (
   id SERIAL PRIMARY KEY,
@@ -15,14 +25,4 @@ CREATE TABLE resources (
   condition_rating INTEGER CHECK (condition_rating >= 1 AND condition_rating <= 5),
   image_url TEXT,
   location_id INTEGER REFERENCES locations(location_id) ON DELETE SET NULL
-);
-
-
--- Table: locations
-CREATE TABLE locations (
-  location_id SERIAL PRIMARY KEY,
-  location_name TEXT NOT NULL,
-  city TEXT,
-  state CHAR(2) CHECK (CHAR_LENGTH(state) = 2),
-  zip_code VARCHAR(5) CHECK (zip_code ~ '^[0-9]{5}$')
 );
--- db/seed.sql
+++ db/seed.sql
@@ -4,15 +4,15 @@
 
 -- Insert resources data
 INSERT INTO resources (name, brand, type, size, is_used, is_available, condition_rating, image_url, location_id) VALUES
-('Winter Wonderland Coat', 'FashionElite', 'Winter Coat', 'Medium', true, true, 4, 'https://example.com/winter-coat-image1.png', 1),
-('Cozy Comfort Jacket', 'SnugStyles', 'Jacket', 'Large', true, true, 4, 'https://example.com/jacket-image1.png', 2),
-('Stylish Trench Coat', 'ChicTrends', 'Trench Coat', 'Small', false, true, 3, 'https://example.com/trench-coat-image1.png', 3),
-('Classic Wool Peacoat', 'TimelessFashion', 'Peacoat', 'Medium', false, true, 4, 'https://example.com/peacoat-image1.png', 1),
-('Sporty Parka', 'OutdoorStyle', 'Parka', 'Large', true, true, 3, 'https://example.com/parka-image1.png', 2),
-('Elegant Evening Coat', 'GlamourCouture', 'Evening Coat', 'Medium', false, true, 4, 'https://example.com/evening-coat-image1.png', 3),
-('Casual Denim Jacket', 'EverydayDenim', 'Denim Jacket', 'Small', false, true, 2, 'https://example.com/denim-jacket-image1.png', 1),
-('Adventurous Explorer Jacket', 'AdventureGear', 'Explorer Jacket', 'Large', true, true, 3, 'https://example.com/explorer-jacket-image1.png', 2),
-('Chic Leather Moto Jacket', 'UrbanStyle', 'Leather Jacket', 'Medium', false, true, 5, 'https://example.com/leather-jacket-image1.png', 3);
+('Winter Wonderland Coat', 'FashionElite', 'Winter Coat', 'Medium', true, true, 4, 'https://pngimg.com/d/coat_PNG3.png', 1),
+('Cozy Comfort Jacket', 'SnugStyles', 'Jacket', 'Large', true, true, 4, 'https://pngimg.com/d/coat_PNG59.png', 2),
+('Stylish Trench Coat', 'ChicTrends', 'Trench Coat', 'Small', false, true, 3, 'https://pngimg.com/d/coat_PNG28.png', 3),
+('Classic Wool Peacoat', 'TimelessFashion', 'Peacoat', 'Medium', false, true, 4, 'https://www.pngall.com/wp-content/uploads/1/Coat-Transparent-Background.png', 1),
+('Sporty Parka', 'OutdoorStyle', 'Parka', 'Large', true, true, 3, 'https://freepngimg.com/thumb/jacket/1-2-jacket-png-clipart.png', 2),
+('Elegant Evening Coat', 'GlamourCouture', 'Evening Coat', 'Medium', false, true, 4, 'https://pngimg.com/d/coat_PNG11.png', 3),
+('Casual Denim Jacket', 'EverydayDenim', 'Denim Jacket', 'Small', false, true, 2, 'https://assets.stickpng.com/images/585680404f6ae202fedf26f0.png', 1),
+('Adventurous Explorer Jacket', 'AdventureGear', 'Explorer Jacket', 'Large', true, true, 3, 'https://e7.pngegg.com/pngimages/807/1021/png-clipart-leather-jacket-flight-jacket-fashion-jacket-zipper-textile.png', 2),
+('Chic Leather Moto Jacket', 'UrbanStyle', 'Leather Jacket', 'Medium', false, true, 5, 'https://w7.pngwing.com/pngs/578/800/png-transparent-leather-jacket-sleeve-jacket-textile-leather-black-thumbnail.png', 3);
 
 -- Insert locations data
 INSERT INTO locations (location_name, city, state, zip_code) VALUES
